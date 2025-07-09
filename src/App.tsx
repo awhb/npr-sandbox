aeac370a2aca7e257452db21e6b6859791351061
awhb <awhangbin@gmail.com>
2025-07-09 18:03:13 +0800

    chore: handle no webgpu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import triangleWgsl from "./shaders/triangle.wgsl?raw";
 
 const App = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [webGpuSupported, setWebGpuSupported] = useState(true);
 
   	const render = async () => {
 		const webGpuContext = await WebGPUContext.create(canvasRef.current!);
 		if (webGpuContext.error) {
 			console.error(webGpuContext.error);
+			setWebGpuSupported(false);
 			return;
 		}
 
@@ -21,6 +23,10 @@ const App = () => {
   }
 
 	useEffect(() => {
+		if (!navigator.gpu) {
+			setWebGpuSupported(false);
+			return;
+		}
 		if (canvasRef.current) {
 			render();
 		}
@@ -29,6 +35,12 @@ const App = () => {
 	return (
 		<div>
 			<canvas ref={canvasRef} width={640} height={480}></canvas>
+			{!webGpuSupported && (
+				<div style={{ color: "red", marginTop: "1rem" }}>
+					Your browser does not support WebGPU. Please download a browser from
+					<a href="https://caniuse.com/webgpu" target="_blank" rel="noopener noreferrer"> this site</a>.
+				</div>
+			)}
 		</div>
 	)
 };
